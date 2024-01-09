190b3ed2f434e28db714c108fa63ea6db349985a
GechCodes <xblurayne30@gmail.com>
2024-01-09 03:27:14 -0800

    restful api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""Define routes for blueprint
-"""
-
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status")
 def status():
-    """Return status of application
-    """
+    '''
+        return JSON of OK status
+    '''
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """Retrieve count of objects in storage
-    """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    json_dict = {}
-
-    for name, cls in classes.items():
-        json_dict.update({name: storage.count(cls)})
-
-    return jsonify(json_dict)
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
 
